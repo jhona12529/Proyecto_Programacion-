2fdc38257eaa9aa5d89db78e254adda8f08b8bf6
Jhonatan Sevillano Chavez <Jhonasevichavez125@gmail.com>
2026-05-24 12:07:28 -0500

    Agregando el codigo de usuario

--- Trabajo.py
+++ Trabajo.py
@@ -1,13 +1,17 @@
-def menu():
-    while True:
-        print("1: Un registro de stock")
-        print("2: Historial ventas")
-        print("3: Un usuario que entre a todas las opciones del menú")
-        print("4: una boleta")
-        print("5: Sistema de clientes frecuentes")
-        print("6: Respaldo // indesuisp")
-        print("7: Salir")
-        opcion = input("Seleccione opcion: ")
-        if opcion == "7":
-            break
-menu()
+
+def jefe():
+    print("Hola Ingrese su usuario y contraseña")
+    usuario = input("Usuario: ")
+    contraseña = input("Contraseña: ")
+    if usuario == "admin" and contraseña == "admin123":
+        print("Bienvenido jefe")
+    else:
+        print("Usuario o contraseña incorrectos")
+def empleado():
+    print("Hola ingrese su usuario y contraseña")
+    usuario = input("Usuario: ")
+    contraseña = input("Contraseña: ")
+    if usuario == "empleado" and contraseña == "empleado123":
+        print("Bienvenido empleado")
+    else:
+        print("Usuario o contraseña incorrectos")
