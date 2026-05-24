6a81817193cbd291f8f91f0e8fa018dc05e6a624
Cristian Alexander <arbildoalexander1@gmail.com>
2026-05-24 12:04:16 -0500

    Codigo agregado

--- Trabajo.py
+++ Trabajo.py
@@ -1,16 +1,16 @@
 def menu():
-    while True:
-        print("1: Un registro de stock")
-        print("2: Historial ventas")
-        print("3: Un usuario que entre a todas las opciones del menú")
-        print("4: una boleta")
-        print("5: Sistema de clientes frecuentes")
-        print("6: Respaldo // indesuisp")
-        print("7: Salir")
-        opcion = input("Seleccione opcion: ")
-        if opcion == "7":
-            break
-menu()
+    def selec():
+        while True:
+            print("*INGRESE OPCION*")
+            print("Jefe")
+            print("Empleado")
+            opcion = input("Selecciona una opcion: ")
+            if opcion == "1":
+                print("aqui aparece el menu del jefe")
+            elif opcion == "2":
+                print("aqui aparece el menu del empleado")
+            else:
+                print("El opcion no existe")
 
 def registro_stock():
     stock_nuevo = []
