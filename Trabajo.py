4bbd799213c6ef288a36bdb06e30b75c98bb60bd
Jhonatan Sevillano Chavez <Jhonasevichavez125@gmail.com>
2026-05-24 11:05:16 -0500

    Modificando mi archivo

---
+++ Trabajo.py
@@ -0,0 +1,13 @@
+def menu():
+    while True:
+        print("1: Un registro de stock")
+        print("2: Historial ventas")
+        print("3: Un usuario que entre a todas las opciones del menú")
+        print("4: una boleta")
+        print("5: Sistema de clientes frecuentes")
+        print("6: Respaldo // indesuisp")
+        print("7: Salir")
+        opcion = input("Seleccione opcion: ")
+        if opcion == "7":
+            break
+menu()
